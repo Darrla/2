f3bafdc88dc682966231fd4ae3817a73c6a4dd02
Daria812021 <udina.daria1@gmail.com>
2024-09-12 01:52:53 +0300

    Добавьте файлы проекта.

---
+++ broo_postav_avtomat_pggg/Program.cs
@@ -0,0 +1,50 @@
+﻿public interface Pplan
+{
+    void release();
+}
+public class Product : Pplan
+{
+    public void release()
+    {
+        Console.WriteLine("tyda syda millioner");
+    }
+}
+public class productplan : Pplan
+{
+    public void release()
+    {
+        Console.WriteLine("tyda syda new millioner");
+    }
+}
+
+public interface goplan
+{
+    Pplan Create();
+}
+ public class productgoplan : goplan
+{
+    public Pplan Create()
+    {
+        return new Product();
+    }
+}
+public class anotherproductgoplan: goplan
+{
+    public Pplan Create()
+    {
+        return new productplan();
+    }
+}
+internal static class program
+{
+    public static void Main(string[] arg)
+    {
+        goplan creator = new productgoplan();
+        Pplan product = creator.Create();
+
+        creator = new anotherproductgoplan();
+        Pplan anotherproduct = creator.Create();
+        product.release();
+        anotherproduct.release();
+    }
+}
